6b017ab477023d086f7a6739b543727ee5fe03d1
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-21 19:35:06 +0530

    "uc1 main"

--- src/com/bridgelabz/datastructure/LinkedListMain.java
+++ src/com/bridgelabz/datastructure/LinkedListMain.java
@@ -1,10 +1,17 @@
 
 package com.bridgelabz.datastructure;
 
-    public class LinkedListMain {
+import static java.lang.System.*;
+
+public class LinkedListMain {
 
         public static void main(String[] args) {
             LinkedList<Integer> linkedList1 = new LinkedList<>();
+            linkedList1.push(70);
+            linkedList1.push(30);
+            linkedList1.push(56);
+
+            System.out.println(linkedList1);
         }
     }
 
