77758c7265dbfb534b03312cafa80d1ac59bc239
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-22 19:01:40 +0530

    "UC4 inserting linkedlist"

--- src/com/bridgelabz/datastructure/LinkedList.java
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -18,13 +18,47 @@ package com.bridgelabz.datastructure;
             head = newNode;
         }
         }
-        public void display() {
+        public void display()
+        {
             Node<T> temp = head;
-            while (temp != null) {
-                System.out.print(temp.key + " -> ");
+            while (temp != null)
+            {
+                if (temp.next == null)
+                {
+                    System.out.print(temp.key);
+                } else
+                {
+                    System.out.print(temp.key + " -> ");
+                }
                 temp = temp.next;
             }
             System.out.println();
+        }
+
+        public void insert(T key,int position){
+            //Create a new node
+            Node<T> newNode = new Node(key);
+            if(head == null) {
+                //If list is empty, both head and tail would point to new node
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                Node temp, current;
+                temp = head;
+                current = null;
+                for(int i = 0; i < position; i++) {
+                    //Node current will point to temp
+                    current = temp;
+                    //Node temp will point to node next to it.
+                    temp = temp.next;
+                }
+                //current will point to new node
+                current.next = newNode;
+                //new node will point to temp
+                newNode.next = temp;
+            }
+
         }
     }
 
