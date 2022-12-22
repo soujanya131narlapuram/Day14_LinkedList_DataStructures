9fca13ae9c61af650eb93e42ad20c0ec40877c71
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-22 19:22:03 +0530

    "UC4 inserting linkedlist"

--- src/com/bridgelabz/datastructure/LinkedList.java
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -1,14 +1,14 @@
 
 package com.bridgelabz.datastructure;
 
-    public class LinkedList<T> {
+public class LinkedList<T> {
 
-        Node<T> head;
-        Node<T> tail;
+    Node<T> head;
+    Node<T> tail;
 
-        public void push(T key) {
-            Node<T> newNode = new Node(key);
-            if(head==null){
+    public void push(T key) {
+        Node<T> newNode = new Node(key);
+        if(head==null){
             head = newNode;
             tail = newNode;
         }
@@ -17,49 +17,49 @@ package com.bridgelabz.datastructure;
             newNode.next = head;
             head = newNode;
         }
-        }
-        public void display()
+    }
+    public void display()
+    {
+        Node<T> temp = head;
+        while (temp != null)
         {
-            Node<T> temp = head;
-            while (temp != null)
+            if (temp.next == null)
             {
-                if (temp.next == null)
-                {
-                    System.out.print(temp.key);
-                } else
-                {
-                    System.out.print(temp.key + " -> ");
-                }
+                System.out.print(temp.key);
+            } else
+            {
+                System.out.print(temp.key + " -> ");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void insert(T key,int position){
+        //Create a new node
+        Node<T> newNode = new Node(key);
+        if(head == null) {
+            //If list is empty, both head and tail would point to new node
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            Node temp, current;
+            temp = head;
+            current = null;
+            for(int i = 0; i < position; i++) {
+                //Node current will point to temp
+                current = temp;
+                //Node temp will point to node next to it.
                 temp = temp.next;
             }
-            System.out.println();
+            //current will point to new node
+            current.next = newNode;
+            //new node will point to temp
+            newNode.next = temp;
         }
 
-        public void insert(T key,int position){
-            //Create a new node
-            Node<T> newNode = new Node(key);
-            if(head == null) {
-                //If list is empty, both head and tail would point to new node
-                head = newNode;
-                tail = newNode;
-            }
-            else {
-                Node temp, current;
-                temp = head;
-                current = null;
-                for(int i = 0; i < position; i++) {
-                    //Node current will point to temp
-                    current = temp;
-                    //Node temp will point to node next to it.
-                    temp = temp.next;
-                }
-                //current will point to new node
-                current.next = newNode;
-                //new node will point to temp
-                newNode.next = temp;
-            }
-
-        }
     }
+}
 
 
